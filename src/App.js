0d9619d0762a7f873534022a64b5b1fb17423a9a
ksj64381 <101380549+ksj64381@users.noreply.github.com>
2022-05-18 23:33:38 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from 'react'
 import {Park} from './Park'
 
 
+const [page, usePage] = useState('');
+
+function onCl = () => usePage((current) => !current)
+
 function App() {
   return (
-    <Park/>
+    <div>
+      <button onClick={onCl()} value={page}>go back <button/>
+    {page ? <Park> : null}
+    </div>
   )
 }
 
